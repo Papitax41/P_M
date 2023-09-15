93a630681bb79df42be5b50f975bdeb4f3f20373
Papita <202000672k@utea.edu.pe>
2023-09-15 07:34:56 -0500

    papita

---
+++ flutter/app_state/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:app_state/home_page.dart';
+import 'package:app_state/test1_page.dart';
+import 'package:flutter/material.dart';
+import 'package:app_state/test2_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Test2Page(),
+    );
+  }
+}
